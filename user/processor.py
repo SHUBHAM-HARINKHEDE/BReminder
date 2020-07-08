0d277d59c53bb2244dd1e2369b9fa6c612b407de
SHUBHAM-HARINKHEDE <harinkhede_shubham.ghrcecs@raisoni.net>
2020-07-08 07:01:38 +0530

    version0.7

--- user/processor.py
+++ user/processor.py
@@ -2,4 +2,6 @@ from user.models import Birthday
 import datetime
 def birthays_this_month(request):
     today = datetime.date.today()
-    return {'birthays_this_month': Birthday.objects.filter(dob__month=today.month,user=request.user)}
+    if user.is_authenticated:
+        return {'birthays_this_month': Birthday.objects.filter(dob__month=today.month,user=request.user)}
+    return {'birthays_this_month': ''}
